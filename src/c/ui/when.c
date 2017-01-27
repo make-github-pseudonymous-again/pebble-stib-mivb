b97336ff4fdc3a4f821bf5ffbd6c6693342bb23c
Aurélien Ooms <aureooms@users.noreply.github.com>
2017-01-27 21:03:58 +0100

    tweak when.c

--- src/c/ui/when.c
+++ src/c/ui/when.c
@@ -1,7 +1,7 @@
 #include <pebble.h>
 #include "when.h"
 
-const int WHEN_GONE_THRESHOLD = -600;
+const int WHEN_GONE_THRESHOLD = -300;
 
 // each string below must have size <= WHEN_BUFFER_SIZE - 1 (see when.h)
 const char *WHEN_IN_MORE_THAN_100_MINUTES = ">99";
@@ -10,11 +10,13 @@ const char *WHEN_NOW = "0";
 
 GColor when (bool quiet, char* buffer, const time_t now, const time_t expected_arrival) {
   
-	// The +5 is to account for data transmission
+	// The +30 is to account for data transmission
 	// and code execution between data retrieval and display.
 	// Should probably send the time of retrieval to avoid
 	// data transmission delay.
-	const time_t seconds = expected_arrival - now + 5 ;
+  // Note that without +30, redrawing the screen just
+  // after loading would remove 1 minute from the counters.
+	const time_t seconds = expected_arrival - now + 30 ;
 
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "%ld %ld %ld", expected_arrival , now , seconds);
 
